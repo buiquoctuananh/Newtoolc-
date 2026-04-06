3d20b1eaba31459be0a3f753fc41610ab8626316
buiquoctuananh <buiquoctuananh2k10@gmail.com>
2026-04-06 08:59:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-print"""Xin lỗi ae!! Tool trên py chính thức dừng hoạt động lúc 11h30 ngày 4/4
-Vui lòng truy cập bit.ly/toolOLM để tiếp tục sử dụng
-Trân trọng cảm ơn ae đã ủng hộ mình"""
+print("""Xin lỗi ae!! Tool trên py chính thức dừng hoạt động lúc 11h30 ngày 4/4
+Vui lòng truy cập toololm.hackertrashman.workers.dev để tiếp tục sử dụng
+Trân trọng cảm ơn ae đã ủng hộ mình""")
