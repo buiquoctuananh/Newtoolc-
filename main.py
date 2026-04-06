367cb33abfad70d00a09e7260615fd7ed2d7b38e
buiquoctuananh <buiquoctuananh2k10@gmail.com>
2026-04-06 12:20:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 print("""Xin lỗi ae!! Tool trên py chính thức dừng hoạt động lúc 11h30 ngày 4/4
-Vui lòng truy cập toololm.hackertrashman.workers.dev để tiếp tục sử dụng
+Vui lòng truy cập 
+toololm.hackertrashman.workers.dev
+để tiếp tục sử dụng
 Trân trọng cảm ơn ae đã ủng hộ mình""")
